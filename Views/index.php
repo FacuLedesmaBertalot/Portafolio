bc8b5f78fd6de1c3ab37fce10e72e28c083d8d58
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-19 18:45:37 -0300

    Añadido v=2.0 a css y js para que siempre limpie el cache en produccion

--- Views/index.php
+++ Views/index.php
@@ -26,7 +26,7 @@ require_once __DIR__ . "/../API/APIProjects.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-  <link rel="stylesheet" href="/Views/css/styles.css">
+  <link rel="stylesheet" href="/Views/css/styles.css=v=2.0">
 </head>
 
 <body class="data-bs-theme">
@@ -307,7 +307,7 @@ require_once __DIR__ . "/../API/APIProjects.php";
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-  <script src="../script.js"></script>
+  <script src="../script.js?v=2.0"></script>
   <script>
     AOS.init();
   </script>
