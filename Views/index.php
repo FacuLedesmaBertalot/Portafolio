be145c2332e766c253cba2778402bcab46a996a1
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-02-19 19:11:29 -0300

    Cambios en estructura y estilos

--- Views/index.php
+++ Views/index.php
@@ -26,7 +26,7 @@ require_once __DIR__ . "/../API/APIProjects.php";
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-  <link rel="stylesheet" href="/Views/css/styles.css=v=2.0">
+  <link rel="stylesheet" href="/Views/css/styles.css?v=2.0">
 </head>
 
 <body class="data-bs-theme">
